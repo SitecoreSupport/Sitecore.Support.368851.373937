2b8d3e7a3bf5d7c4741bffa601e7cb582f1d1b50
PaulStupka <stupka.paul@gmail.com>
2021-03-15 19:07:45 +0200

    clean up unused references
    (cherry picked from commit 48a43e08824d35c989d88864182cd2e5c9fdf0dc)

--- src/Sitecore.Support.368851.373937/CheckSitecoreQuery.cs
+++ src/Sitecore.Support.368851.373937/CheckSitecoreQuery.cs
@@ -1,10 +1,6 @@
 ﻿using Sitecore.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Sitecore.XA.Foundation.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Sitecore.XA.Foundation.Multisite.Extensions;
 using Sitecore.Pipelines.ParseDataSource;
 
--- src/Sitecore.Support.368851.373937/SearchDatasource.cs
+++ src/Sitecore.Support.368851.373937/SearchDatasource.cs
@@ -1,8 +1,4 @@
 ﻿using Sitecore.Pipelines.ResolveRenderingDatasource;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Sitecore.Support
 {
